8be85da859690f87105078cbcd7e98fea84b97b8
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-08-14 02:14:55 +0530

    pop

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import express, { type Request, type Response } from "express"
 const app = express()
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "hello v1" })
+  res.json({ message: "hello test 2" })
 })
 
 const port = process.env.PORT || 3000

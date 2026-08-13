66e45f9189921815caf79081f420db26a00cfd38
Pratham Gupta <prathamgupta.wk@gmail.com>
2026-08-13 16:15:59 +0530

    init

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import "dotenv/config"
+import express, { type Request, type Response } from "express"
+
+const app = express()
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({ message: "hello v1" })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server on port ${port}`))
